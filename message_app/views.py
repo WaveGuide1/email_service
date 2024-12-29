28fcd38cab0e4fe7275a93126ee857e34bf645f4
WaveGuide1 <barthalaekwe@gmail.com>
2024-12-29 07:00:30 +0100

    Message board model and form setup

--- message_app/views.py
+++ message_app/views.py
@@ -1,9 +1,30 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from message_app.forms import MessageForm
+from message_app.models import MessageBoard
 
 
 # Create your views here.
 
 @login_required
 def message_view(request):
-    return render(request, 'a_message/index.html')
+    messageboard = get_object_or_404(MessageBoard, id=1)
+    form = MessageForm()
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.author = request.user
+            message.messageboard = messageboard
+            message.save()
+
+        else:
+            messages.warning(request, 'Subscribe first')
+
+        return redirect('messageboard')
+
+    context = {'messageboard': messageboard, 'form': form}
+    return render(request, 'a_message/index.html', context)
